3029e5bfdc12437516176bf978f3e672a6ca84ac
Michele Renda <michele.renda@gmail.com>
2016-01-18 15:45:52 +0200

    Moved to uIP

--- bambu.sh
+++ bambu.sh
@@ -4,16 +4,18 @@ rm -rf build
 mkdir -p build
 
 BASE=`pwd`
-LWIP=${BASE}/lwip-1.4.1
+UCIP=${BASE}/uip
 
 cd build
 
 echo "#synthesis of micorserver and LwIP"
-bambu \
-	${BASE}/src/microserver.c			\
-	${LWIP}/src/core/*.c 				\
-	${LWIP}/src/api/*.c  				\
-	-I${LWIP}/src/include/				\
-	-I${LWIP}/src/include/ipv4/			\
+bambu --std=c11 --device-name=xc7vx485t-2ffg1761-VVD -v2 \
 	-I${BASE}/src/						\
-	--std=c11 --device-name=xc7vx485t-2ffg1761-VVD -v2 2>&1 | tee build.log
+	-I${UCIP}/uip/						\
+	${BASE}/src/microserver.c			\
+	${BASE}/src/clock-arch.c			\
+	${UCIP}/uip/*.c						\
+ 2>&1 | tee build.log
+
+
+# ${BASE}/src/microserver.c			\
--- src/microserver.c
+++ src/microserver.c
@@ -1,5 +1,16 @@
+#include "microserver.h"
 
-int microserver_main(
+
+
+
+#define LOCAL_ADDRS   "192.168.0.10"
+#define NETWORK_MASK  "255.255.255.0"
+#define DEFAULT_ROUTE "192.168.0.1"
+
+
+
+	
+void microserver(
 	unsigned char* 	gmii_txd,           
     unsigned char*  gmii_tx_en,  
     unsigned char*  gmii_tx_er,     
@@ -8,7 +19,6 @@ int microserver_main(
     unsigned char*  gmii_rx_er, 
     unsigned short*	gmii_status)
 {
-	// This is just an example.
-	//(*gmii_txd) = 0;
-
+	
+	
 }
