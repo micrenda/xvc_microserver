6e5469a69cffd1a26c5a3df3b48381e6a1bc1837
Michele Renda <michele.renda@gmail.com>
2016-02-04 12:17:30 +0200

    Fixing bitgen script

--- bambu-bitgen.sh
+++ bambu-bitgen.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
-VIVADO_VER=2015.1/
+if [[ -z "$VIVADO_VER" ]]
+then
+	VIVADO_VER="2015.4"
+fi
 
-. /opt/Xilinx/Vivado/${VIVADO_VER}/settings64.sh >& /dev/null; 
+if [[ -z "$VIVADO_DIR" ]]
+then
+	VIVADO_DIR="/opt/Xilinx/Vivado/${VIVADO_VER}"
+fi
+
+if [[ ! -d $VIVADO_DIR ]]
+then
+	echo "Unable to find the Vivado directory: $VIVADO_DIR"
+	echo "Please define VIVADO_VER or VIVADO_DIR enviroment variable"
+	exit 1;
+fi
+
+. ${VIVADO_DIR}/settings64.sh >& /dev/null;
 
 vivado -mode batch -nojournal -nolog -source bitgen.tcl
