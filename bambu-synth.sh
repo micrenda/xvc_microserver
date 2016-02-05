3f490492477196ce3dbacb8b7db7fdeb1810e250
Michele Renda <michele.renda@gmail.com>
2016-02-05 09:16:46 +0200

    Modified build script

--- bambu-synth.sh
+++ bambu-synth.sh
@@ -15,26 +15,23 @@ GIG_ETH_PCS_PMA=ip_cores/gig_eth_pcs_pma_v11_5/example_design/gig_eth_pcs_pma_v1
 cd synth
 
 echo "#synthesis of micorserver and uIP"
-bambu -O3 -v5 --std=c11 --device-name=$DEVICE   --top-fname=main	\
-	--backend-sdc-extensions=$BOARD/master.sdc 						\
-	--backend-script-extensions=gig_eth_pcs_pma_v11.inc				\
-	--clock-period=$CLK_PERIOD 										\
-	--file-input-data=${BASE}/src/microserver.v,${BASE}/src/driver.v,${BASE}/src/clock-arch.v --evaluation --objective=PERIOD,AREA,FREQUENCY,CLOCK_SLACK,REGISTERS,DSPS,BRAMS	\
-	-I${BASE}/src/						\
-	-I${UCIP}/uip/						\
-	${BASE}/src/constraints_STD.xml		\
-	${BASE}/src/IPs.xml					\
-	${BASE}/src/microserver.c			\
-	${BASE}/src/driver.c				\
-	${BASE}/src/net-dev.c				\
-	${UCIP}/uip/uip_arp.c				\
-	${UCIP}/uip/timer.c					\
-	${UCIP}/uip/uip.c					\
-	${UCIP}/uip/uiplib.c				\
-	${UCIP}/uip/psock.c					\
- 2>&1 | tee build.log
-
-
-# 	${UCIP}/uip/uip-fw.c				\
-#	${UCIP}/uip/uip-neighbor.c			\
-#	${UCIP}/uip/uip-split.c				
+bambu -O3 -v5 --std=c11 --device-name=$DEVICE   --top-fname=main		\
+	--backend-sdc-extensions=$BOARD/master.sdc 							\
+	--backend-script-extensions=gig_eth_pcs_pma_v11.inc					\
+	--clock-period=$CLK_PERIOD 											\
+	--file-input-data=${BASE}/src/microserver.v,${BASE}/src/driver.v,${BASE}/src/clock-arch.v \
+	--evaluation 														\
+	--objective=PERIOD,AREA,FREQUENCY,CLOCK_SLACK,REGISTERS,DSPS,BRAMS	\
+	-I${BASE}/src/														\
+	-I${UCIP}/uip/														\
+	${BASE}/src/constraints_STD.xml										\
+	${BASE}/src/IPs.xml													\
+	${BASE}/src/microserver.c											\
+	${BASE}/src/driver.c												\
+	${BASE}/src/net-dev.c												\
+	${UCIP}/uip/uip_arp.c												\
+	${UCIP}/uip/timer.c													\
+	${UCIP}/uip/uip.c													\
+	${UCIP}/uip/uiplib.c												\
+	${UCIP}/uip/psock.c													\
+ 2>&1 | tee synth.log
