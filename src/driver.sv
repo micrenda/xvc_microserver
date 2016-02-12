66f6825236444fcb1eb31397d7733de384c0ca8a
Michele Renda <michele.renda@gmail.com>
2016-02-12 19:15:45 +0200

    Rename constants

--- src/driver.sv
+++ src/driver.sv
@@ -24,97 +24,85 @@ typedef TypePacketAddr   TypeWrBufLen [0:`BUFFER_SIZE_WR-1];
 typedef TypeByte         TypeWrBuf    [0:`BUFFER_SIZE_WR-1][0:`PACKET_SIZE-1];
 
 
-var TypeRdBufLen rd_buf_len;
-var TypeRdBuf    rd_buf;
-
-var TypeWrBufLen wr_buf_len;
-var TypeWrBuf    wr_buf;
-
-var TypeBufferWrAddr buf_last_sent;
-var TypeBufferWrAddr buf_last_wrote;
-var TypeBufferRdAddr buf_last_recv;
-var TypeBufferRdAddr buf_last_read;
-
-
-`define READY      0
-`define PREAMBLE_0 1
-`define PREAMBLE_1 2
-`define PREAMBLE_2 3
-`define PREAMBLE_3 4
-`define PREAMBLE_4 5
-`define PREAMBLE_5 6
-`define PREAMBLE_6 7
-`define SDF        8
-`define DATA       9
-`define SEND_CRC_3 10
-`define SEND_CRC_2 11
-`define SEND_CRC_1 12
-`define SEND_CRC_0 13
-`define DONE       14
-`define INTERGAP   15
+`define STATUS_READY      0
+`define STATUS_PREAMBLE_0 1
+`define STATUS_PREAMBLE_1 2
+`define STATUS_PREAMBLE_2 3
+`define STATUS_PREAMBLE_3 4
+`define STATUS_PREAMBLE_4 5
+`define STATUS_PREAMBLE_5 6
+`define STATUS_PREAMBLE_6 7
+`define STATUS_SDF        8
+`define STATUS_DATA       9
+`define STATUS_SEND_CRC_3 10
+`define STATUS_SEND_CRC_2 11
+`define STATUS_SEND_CRC_1 12
+`define STATUS_SEND_CRC_0 13
+`define STATUS_DONE       14
+`define STATUS_INTERGAP   15
     
     
     
-    // polynomial: (0 1 2 4 5 7 8 10 11 12 16 22 23 26 32)
-    // data width: 8
-    // convention: the first serial bit is d[0]
-    function int next_crc32_d8;
-        input [7:0] data;
-        input [31:0] crc;
-        reg[7:0]  d;
-        reg[31:0] c;
-    begin
-        d = data;
-        c = crc;
-        next_crc32_d8[0]  = c[24] ^ c[30] ^ d[1]  ^ d[7];
-        next_crc32_d8[1]  = c[25] ^ c[31] ^ d[0]  ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
-        next_crc32_d8[2]  = c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
-        next_crc32_d8[3]  = c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
-        next_crc32_d8[4]  = c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
-        next_crc32_d8[5]  = c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
-        next_crc32_d8[6]  = c[30] ^ d[1]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
-        next_crc32_d8[7]  = c[31] ^ d[0]  ^ c[29] ^ d[2]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[24] ^ d[7];
-        next_crc32_d8[8]  = c[0]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[25] ^ d[6]  ^ c[24] ^ d[7];
-        next_crc32_d8[9]  = c[1]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[26] ^ d[5]  ^ c[25] ^ d[6];
-        next_crc32_d8[10] = c[2]  ^ c[29] ^ d[2]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[24] ^ d[7];
-        next_crc32_d8[11] = c[3]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[25] ^ d[6]  ^ c[24] ^ d[7];
-        next_crc32_d8[12] = c[4]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[26] ^ d[5]  ^ c[25] ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
-        next_crc32_d8[13] = c[5]  ^ c[30] ^ d[1]  ^ c[29] ^ d[2]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
-        next_crc32_d8[14] = c[6]  ^ c[31] ^ d[0]  ^ c[30] ^ d[1]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5];
-        next_crc32_d8[15] = c[7]  ^ c[31] ^ d[0]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4];
-        next_crc32_d8[16] = c[8]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[24] ^ d[7];
-        next_crc32_d8[17] = c[9]  ^ c[30] ^ d[1]  ^ c[29] ^ d[2]  ^ c[25] ^ d[6];
-        next_crc32_d8[18] = c[10] ^ c[31] ^ d[0]  ^ c[30] ^ d[1]  ^ c[26] ^ d[5];
-        next_crc32_d8[19] = c[11] ^ c[31] ^ d[0]  ^ c[27] ^ d[4];
-        next_crc32_d8[20] = c[12] ^ c[28] ^ d[3];
-        next_crc32_d8[21] = c[13] ^ c[29] ^ d[2];
-        next_crc32_d8[22] = c[14] ^ c[24] ^ d[7];
-        next_crc32_d8[23] = c[15] ^ c[25] ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
-        next_crc32_d8[24] = c[16] ^ c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
-        next_crc32_d8[25] = c[17] ^ c[27] ^ d[4]  ^ c[26] ^ d[5];
-        next_crc32_d8[26] = c[18] ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
-        next_crc32_d8[27] = c[19] ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
-        next_crc32_d8[28] = c[20] ^ c[30] ^ d[1]  ^ c[29] ^ d[2]  ^ c[26] ^ d[5];
-        next_crc32_d8[29] = c[21] ^ c[31] ^ d[0]  ^ c[30] ^ d[1]  ^ c[27] ^ d[4];
-        next_crc32_d8[30] = c[22] ^ c[31] ^ d[0]  ^ c[28] ^ d[3];
-        next_crc32_d8[31] = c[23] ^ c[29] ^ d[2];
-    end
-    endfunction
+// polynomial: (0 1 2 4 5 7 8 10 11 12 16 22 23 26 32)
+// data width: 8
+// convention: the first serial bit is d[0]
+function int next_crc32_d8;
+	input [7:0] data;
+	input [31:0] crc;
+	reg[7:0]  d;
+	reg[31:0] c;
+begin
+	d = data;
+	c = crc;
+	next_crc32_d8[0]  = c[24] ^ c[30] ^ d[1]  ^ d[7];
+	next_crc32_d8[1]  = c[25] ^ c[31] ^ d[0]  ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
+	next_crc32_d8[2]  = c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
+	next_crc32_d8[3]  = c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
+	next_crc32_d8[4]  = c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
+	next_crc32_d8[5]  = c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
+	next_crc32_d8[6]  = c[30] ^ d[1]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
+	next_crc32_d8[7]  = c[31] ^ d[0]  ^ c[29] ^ d[2]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[24] ^ d[7];
+	next_crc32_d8[8]  = c[0]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[25] ^ d[6]  ^ c[24] ^ d[7];
+	next_crc32_d8[9]  = c[1]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[26] ^ d[5]  ^ c[25] ^ d[6];
+	next_crc32_d8[10] = c[2]  ^ c[29] ^ d[2]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[24] ^ d[7];
+	next_crc32_d8[11] = c[3]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[25] ^ d[6]  ^ c[24] ^ d[7];
+	next_crc32_d8[12] = c[4]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[26] ^ d[5]  ^ c[25] ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
+	next_crc32_d8[13] = c[5]  ^ c[30] ^ d[1]  ^ c[29] ^ d[2]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
+	next_crc32_d8[14] = c[6]  ^ c[31] ^ d[0]  ^ c[30] ^ d[1]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[26] ^ d[5];
+	next_crc32_d8[15] = c[7]  ^ c[31] ^ d[0]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[27] ^ d[4];
+	next_crc32_d8[16] = c[8]  ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[24] ^ d[7];
+	next_crc32_d8[17] = c[9]  ^ c[30] ^ d[1]  ^ c[29] ^ d[2]  ^ c[25] ^ d[6];
+	next_crc32_d8[18] = c[10] ^ c[31] ^ d[0]  ^ c[30] ^ d[1]  ^ c[26] ^ d[5];
+	next_crc32_d8[19] = c[11] ^ c[31] ^ d[0]  ^ c[27] ^ d[4];
+	next_crc32_d8[20] = c[12] ^ c[28] ^ d[3];
+	next_crc32_d8[21] = c[13] ^ c[29] ^ d[2];
+	next_crc32_d8[22] = c[14] ^ c[24] ^ d[7];
+	next_crc32_d8[23] = c[15] ^ c[25] ^ d[6]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
+	next_crc32_d8[24] = c[16] ^ c[26] ^ d[5]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
+	next_crc32_d8[25] = c[17] ^ c[27] ^ d[4]  ^ c[26] ^ d[5];
+	next_crc32_d8[26] = c[18] ^ c[28] ^ d[3]  ^ c[27] ^ d[4]  ^ c[24] ^ c[30] ^ d[1]  ^ d[7];
+	next_crc32_d8[27] = c[19] ^ c[29] ^ d[2]  ^ c[28] ^ d[3]  ^ c[25] ^ c[31] ^ d[0]  ^ d[6];
+	next_crc32_d8[28] = c[20] ^ c[30] ^ d[1]  ^ c[29] ^ d[2]  ^ c[26] ^ d[5];
+	next_crc32_d8[29] = c[21] ^ c[31] ^ d[0]  ^ c[30] ^ d[1]  ^ c[27] ^ d[4];
+	next_crc32_d8[30] = c[22] ^ c[31] ^ d[0]  ^ c[28] ^ d[3];
+	next_crc32_d8[31] = c[23] ^ c[29] ^ d[2];
+end
+endfunction
 
 
-    function [7:0] reverse_byte;
-        input[7:0] data;
-    begin
-        reverse_byte[0] = data[7];
-        reverse_byte[1] = data[6];
-        reverse_byte[2] = data[5];
-        reverse_byte[3] = data[4];
-        reverse_byte[4] = data[3];
-        reverse_byte[5] = data[2];
-        reverse_byte[6] = data[1];
-        reverse_byte[7] = data[0];
-    end
-    endfunction
+function [7:0] reverse_byte;
+	input[7:0] data;
+begin
+	reverse_byte[0] = data[7];
+	reverse_byte[1] = data[6];
+	reverse_byte[2] = data[5];
+	reverse_byte[3] = data[4];
+	reverse_byte[4] = data[3];
+	reverse_byte[5] = data[2];
+	reverse_byte[6] = data[1];
+	reverse_byte[7] = data[0];
+end
+endfunction
 
 
 
@@ -326,6 +314,16 @@ module driver_operation(
 
 
 
+	var TypeRdBufLen rd_buf_len;
+	var TypeRdBuf    rd_buf;
+
+	var TypeWrBufLen wr_buf_len;
+	var TypeWrBuf    wr_buf;
+
+	var TypeBufferWrAddr buf_last_sent;
+	var TypeBufferWrAddr buf_last_wrote;
+	var TypeBufferRdAddr buf_last_recv;
+	var TypeBufferRdAddr buf_last_read;
 
     
     
@@ -576,13 +574,13 @@ module handle_tx(
     always @(posedge clock)
     if (reset)
     begin
-        state_tx <= `READY;
+        state_tx <= `STATUS_READY;
         tx_en         <= 0;
         tx_er         <= 0;
     end else begin
         
         case (state_tx)
-            `READY:
+            `STATUS_READY:
                 if (buf_last_sent != buf_last_wrote)
                 begin
                     pkg_current     <= 0;
@@ -590,90 +588,90 @@ module handle_tx(
                     crc32_tx        <= 0;
                     tx_intergap     <= 0;
                     tx_en           <= 0;
-                    state_tx        <= `PREAMBLE_0;
+                    state_tx        <= `STATUS_PREAMBLE_0;
                 end
                 
-            `PREAMBLE_0:
+            `STATUS_PREAMBLE_0:
                 begin
                     tx_en <= 1;
                     tx_data <= 8'h55;
-                    state_tx <= `PREAMBLE_1;
+                    state_tx <= `STATUS_PREAMBLE_1;
                 end
             
-            `PREAMBLE_1:
-                state_tx <= `PREAMBLE_2;
+            `STATUS_PREAMBLE_1:
+                state_tx <= `STATUS_PREAMBLE_2;
             
-            `PREAMBLE_2:
-                state_tx <= `PREAMBLE_3;
+            `STATUS_PREAMBLE_2:
+                state_tx <= `STATUS_PREAMBLE_3;
             
-            `PREAMBLE_3:
-                state_tx <= `PREAMBLE_4;
+            `STATUS_PREAMBLE_3:
+                state_tx <= `STATUS_PREAMBLE_4;
             
-            `PREAMBLE_4:
-                state_tx <= `PREAMBLE_5;
+            `STATUS_PREAMBLE_4:
+                state_tx <= `STATUS_PREAMBLE_5;
             
-            `PREAMBLE_5:
-                state_tx <= `PREAMBLE_6;
+            `STATUS_PREAMBLE_5:
+                state_tx <= `STATUS_PREAMBLE_6;
             
-            `PREAMBLE_6:
-                state_tx <= `SDF;
+            `STATUS_PREAMBLE_6:
+                state_tx <= `STATUS_SDF;
             
-            `SDF:
+            `STATUS_SDF:
                 begin
                     tx_data <= 8'hd5;
-                    state_tx <= `DATA;
+                    state_tx <= `STATUS_DATA;
                 end
                 
-            `DATA:
+            `STATUS_DATA:
                 if (pkg_current < wr_buf_len[buf_current])
                 begin
                     tx_data       <= wr_buf[buf_current][pkg_current];
                     crc32_tx      <= next_crc32_d8(wr_buf[buf_current][pkg_current], crc32_tx);
                     pkg_current   <= pkg_current + 1;
                 end else begin
-                    state_tx      <= `SEND_CRC_3;
+                    state_tx      <= `STATUS_SEND_CRC_3;
                 end
                 
-            `SEND_CRC_3:
+            `STATUS_SEND_CRC_3:
                 begin
                     tx_data  <= ~reverse_byte(crc32_tx[31:24]);
-                    state_tx <= `SEND_CRC_2;
+                    state_tx <= `STATUS_SEND_CRC_2;
                 end
                 
-            `SEND_CRC_2:
+            `STATUS_SEND_CRC_2:
                 begin
                     tx_data  <= ~reverse_byte(crc32_tx[23:16]);
-                    state_tx <= `SEND_CRC_1;
+                    state_tx <= `STATUS_SEND_CRC_1;
                 end
                 
-            `SEND_CRC_1:
+            `STATUS_SEND_CRC_1:
                 begin
                     tx_data  <= ~reverse_byte(crc32_tx[15:8]);
-                    state_tx <= `SEND_CRC_0;
+                    state_tx <= `STATUS_SEND_CRC_0;
                 end
                 
-            `SEND_CRC_0:
+            `STATUS_SEND_CRC_0:
                 begin
                     tx_data  <= ~reverse_byte(crc32_tx[7:0]);
-                    state_tx <= `DONE;
+                    state_tx <= `STATUS_DONE;
                 end
                 
-            `DONE:
+            `STATUS_DONE:
                 begin
                     buf_last_sent <= buf_current;
-                    state_tx <= `INTERGAP;
+                    state_tx <= `STATUS_INTERGAP;
                 end
                 
-            `INTERGAP:
+            `STATUS_INTERGAP:
                 if (tx_intergap < 12)
                 begin
                     tx_intergap <= tx_intergap + 1;
                 end else begin
-                    state_tx <= `READY;
+                    state_tx <= `STATUS_READY;
                 end
             
             default:
-                state_tx <= `READY;
+                state_tx <= `STATUS_READY;
         endcase 
     end
 endmodule
@@ -702,81 +700,81 @@ module handle_rx(
     always @(posedge clock)
     if (reset)
     begin
-        state_rx <= `READY;
+        state_rx <= `STATUS_READY;
     end else begin
         
         if (!rx_dv | rx_er) begin
         
-            state_rx    <= `READY;
+            state_rx    <= `STATUS_READY;
             
         end else begin
         
             case (state_rx)
-                `READY:
+                `STATUS_READY:
                     if (rx_data == 8'h55) begin
                         buf_current     <= buf_last_recv + 1;
                         crc32_rx        <= 0;
-                        state_rx        <= `PREAMBLE_0;
+                        state_rx        <= `STATUS_PREAMBLE_0;
                     end
                     
                     
-                `PREAMBLE_0:
+                `STATUS_PREAMBLE_0:
                     if (rx_data == 8'h55) begin
-                        state_rx <= `PREAMBLE_1;
+                        state_rx <= `STATUS_PREAMBLE_1;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                 
-                `PREAMBLE_1:
+                `STATUS_PREAMBLE_1:
                     if (rx_data == 8'h55) begin
-                        state_rx <= `PREAMBLE_2;
+                        state_rx <= `STATUS_PREAMBLE_2;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                 
-                `PREAMBLE_2:
+                `STATUS_PREAMBLE_2:
                     if (rx_data == 8'h55) begin
-                        state_rx <= `PREAMBLE_3;
+                        state_rx <= `STATUS_PREAMBLE_3;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                 
-                `PREAMBLE_3:
+                `STATUS_PREAMBLE_3:
                     if (rx_data == 8'h55) begin
-                        state_rx <= `PREAMBLE_4;
+                        state_rx <= `STATUS_PREAMBLE_4;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                 
-                `PREAMBLE_4:
+                `STATUS_PREAMBLE_4:
                     if (rx_data == 8'h55) begin
-                        state_rx <= `PREAMBLE_5;
+                        state_rx <= `STATUS_PREAMBLE_5;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                 
-                `PREAMBLE_5:
+                `STATUS_PREAMBLE_5:
                     if (rx_data == 8'h55) begin
-                        state_rx <= `PREAMBLE_6;
+                        state_rx <= `STATUS_PREAMBLE_6;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                 
-                `PREAMBLE_6:
+                `STATUS_PREAMBLE_6:
                     if (rx_data == 8'h55) begin
-                        state_rx <= `SDF;
+                        state_rx <= `STATUS_SDF;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                 
-                `SDF:
+                `STATUS_SDF:
                     if (rx_data == 8'hd5) begin
-                        state_rx <= `DATA;
+                        state_rx <= `STATUS_DATA;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                     
-                `DATA:
+                `STATUS_DATA:
                     if (pkg_current < `PACKET_SIZE)
                     begin
                         
@@ -787,21 +785,21 @@ module handle_rx(
                             pkg_current   <= pkg_current + 1;
                             
                         end else begin
-                            state_rx <= `DONE;
+                            state_rx <= `STATUS_DONE;
                         end;
                     end else begin
-                        state_rx <= `READY;
+                        state_rx <= `STATUS_READY;
                     end
                     
-                `DONE:
+                `STATUS_DONE:
                     begin
                         buf_last_recv           <= buf_current;
                         rd_buf_len[pkg_current] <= pkg_current - 4;
-                        state_rx                <= `READY;
+                        state_rx                <= `STATUS_READY;
                     end
                     
                 default:
-                    state_rx      <= `READY;
+                    state_rx      <= `STATUS_READY;
             endcase 
         end
     end
