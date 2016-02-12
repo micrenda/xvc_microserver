7e8e9a88e240810dbb272029803f0ab60928bb44
Michele Renda <michele.renda@gmail.com>
2016-02-12 17:57:54 +0200

    Fix mistake in net-if

--- src/net-dev.c
+++ src/net-dev.c
@@ -14,7 +14,7 @@ void network_device_send(void)
 	if (uip_len > 0)
 	{
 		for (uint16_t i = 0; i < uip_len; i++)
-			write_buffer(uip_buf[i]);
+			write_buffer(i, uip_buf[i]);
 
 		if (write_buffer_next() != 0)
 		{
@@ -33,7 +33,7 @@ uint16_t network_device_read(void)
 	
 	for (uint16_t i = 0; i < uip_len; i++)
 	{
-		uip_buf[uip_len] = read_buffer();
+		uip_buf[i] = read_buffer(i);
 	}
 	
 	return uip_len;
