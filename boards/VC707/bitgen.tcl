3998d8b6634b7821f17249412a506c169c02668c
Michele Renda <michele.renda@gmail.com>
2016-04-08 13:05:08 +0300

    Fix typos

--- boards/VC707/bitgen.tcl
+++ boards/VC707/bitgen.tcl
@@ -2,4 +2,4 @@
 open_checkpoint synth/HLS_output/Synthesis/vivado_flow/post_route.dcp
 source boards/VC707/master.sdc
 source boards/VC707/gig_ethernet_pcs_pma_0.xdc
-write_bitstream -force bistream.bit
+write_bitstream -force bitstream.bit
